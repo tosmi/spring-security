e866b1c0d5455e9fb98a26e98067dd25152107d4
Toni Schmidbauer <toni@stderr.at>
2021-10-05 21:04:45 +0200

    import ex2

---
+++ ssia-ch2-ex2/src/main/java/at/stderr/ssiach2ex2/HelloController.java
@@ -0,0 +1,13 @@
+package at.stderr.ssiach2ex2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+	return "Hello!";
+    }
+}
